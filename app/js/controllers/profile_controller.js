93efef97995f988d7d0e77458f0d5a653073eeee
banafederico <federico@neo.com>
2013-10-15 16:34:15 -0200

    Better use of profile parts.

--- app/js/controllers/profile_controller.js
+++ app/js/controllers/profile_controller.js
@@ -1,3 +1,17 @@
 define(['application'], function (App) {
-  App.ProfileController = Ember.Controller.extend();
+  App.ProfileController = Ember.Controller.extend({
+
+    emailLink: (function () {
+      return 'mailto:' + this.get('person.email');
+    }).property('person.email'),
+
+    githubLink: (function () {
+      return 'http://github.com/' + this.get('person.github');2
+    }).property('person.github'),
+
+    twitterLink: (function () {
+      return 'http://twitter.com/' + this.get('person.twitter');
+    }).property('person.twitter')
+
+  });
 });
