2204998206d86da68065ea7ca10405980d2137d5
Alvaro Gil <zevarito@gmail.com>
2013-10-23 10:38:26 -0200

    add iframe of t2 ecosystem

--- app/js/application.js
+++ app/js/application.js
@@ -5,14 +5,14 @@ define(function () {
   App.AUTH_TOKEN = document.location.href.match(/authentication_token=([a-zA-Z0-9]+)/)
   App.AUTH_TOKEN = App.AUTH_TOKEN ? App.AUTH_TOKEN[1] : null;
 
-  if (isDev) {
-    App.API_BASE_URL = 'http://localhost:5000/api/v1';
-    App.SIGN_IN_URL  = 'http://localhost:5000/sign_in';
-  }
-  else {
-    App.API_BASE_URL = 'http://t2api.herokuapp.com/api/v1';
-    App.SIGN_IN_URL  = 'http://t2api.herokuapp.com/sign_in';
-  }
+  if (isDev)
+    App.API_HOST = 'http://localhost:5000';
+  else
+    App.API_HOST = 'http://t2api.herokuapp.com';
+
+  App.API_BASE_URL = App.API_HOST + '/api/v1';
+  App.SIGN_IN_URL  = App.API_HOST + '/sign_in';
+  App.NAVBAR_URL   = App.API_HOST + '/navbar';
 
   window.App = App;
   return App;
