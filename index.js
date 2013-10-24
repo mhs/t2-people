f894af60a3768a88b859129bee253299d8739722
banafederico <federico@neo.com>
2013-10-24 11:13:57 -0200

    Change __dirname for process.cwd() because Heroku dont like that.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var connect = require('connect');
 
 var app = connect();
-app.use(connect.logger('dev'));
-app.use(connect.static(__dirname));
+app.use(connect.static(process.cwd()));
 app.listen(process.env.PORT);
