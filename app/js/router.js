6d05e88fdaea46063b1597e080f352c35e155191
banafederico <federico@neo.com>
2013-10-25 11:35:05 -0200

    Include skills_route to the dependency tree.

--- app/js/router.js
+++ app/js/router.js
@@ -3,7 +3,8 @@ define(
     'application',
     'helpers',
     'routes/application_route',
-    'routes/profile_route'
+    'routes/profile_route',
+    'routes/skills_route'
   ],
   function (App) {
     App.Router.map(function () {
