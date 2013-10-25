9fb8149228422b7f785b0a3f178cea8b2c06d689
banafederico <federico@neo.com>
2013-10-25 16:07:24 -0200

    Refactor Raw2EmberObject to reach deep properties.

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -1,5 +1,19 @@
 define(['application'], function (App) {
-  App.Raw2EmberObject = function (object) {
-    return Ember.Object.create(object);
+  App.Raw2EmberObject = function (rawObject) {
+
+    /* Recursively transform deep properties
+       into Ember.Object(s) */
+    for (var i in rawObject) {
+      if (rawObject.hasOwnProperty(i)) {
+        if (Ember.typeOf(rawObject[i]) === 'object') {
+          rawObject[i] = App.Raw2EmberObject(rawObject[i]);
+        }
+        else if (Ember.typeOf(rawObject[i]) === 'array') {
+          rawObject[i] = rawObject[i].map(App.Raw2EmberObject);
+        }
+      }
+    }
+
+    return Ember.Object.create(rawObject);
   }
 })
