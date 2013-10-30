7a2b52389cc7b0d7e81a4a1a0a04c79a1639e38d
banafederico <federico@neo.com>
2013-10-30 18:07:06 -0200

    Styled list for people search.

--- app/js/controllers/search_controller.js
+++ app/js/controllers/search_controller.js
@@ -1,3 +1,10 @@
 define(['application'], function (App) {
-  App.SearchController = Ember.Controller.extend();
+  App.SearchController = Ember.Controller.extend({
+    filteredPeople: (function () {
+      var keyword = this.get('keyword');
+      return this.get('people').filter(function (person) {
+        return person.get('name').toLowerCase().indexOf(keyword) != -1;
+      })
+    }).property('people')
+  });
 });
