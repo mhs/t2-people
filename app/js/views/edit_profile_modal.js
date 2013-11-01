e2d1b5cff40e7df9b4870492d3fca2609bb5b6fa
Alvaro Gil <zevarito@gmail.com>
2013-11-01 15:12:43 -0200

    add simple avatar upload

--- app/js/views/edit_profile_modal.js
+++ app/js/views/edit_profile_modal.js
@@ -4,19 +4,27 @@ define(["application"], function(App) {
       save: function () {
         var person = this.get('controller.person')
           , personData = this.get('controller.personData')
-          , newProperties = personData.getProperties([
-              'name', 'title', 'bio', 'website', 'github', 'twitter'
-            ]);
+          , properties = ['name', 'title', 'bio', 'website', 'github', 'twitter']
+          , newProperties = personData.getProperties(properties);
 
         /* Hide modal. */
         this.$('#edit_profile_modal').modal('hide');
 
+        var data = new FormData();
+
+        properties.forEach(function (i) {
+          data.append("person[" + i + "]", newProperties[i]);
+        });
+
+        var file = this.$('input[type=file]')[0].files[0]
+        data.append('person[avatar]', file || null);
+
         $.ajax({
           url: App.API_BASE_URL + '/profile',
           type: 'put',
-          data: {
-            person: newProperties
-          }
+          data: data,
+          contentType: false,
+          processData: false
         }).done(function () {
           person.setProperties(newProperties);
         });
