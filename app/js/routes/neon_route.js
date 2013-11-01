41ef01f9c3ff8e8c196ed0c82341c5d0cef524c4
banafederico <federico@neo.com>
2013-11-01 11:47:57 -0200

    Fix person load in neon route.

--- app/js/routes/neon_route.js
+++ app/js/routes/neon_route.js
@@ -11,15 +11,12 @@ define(
         // route is ready on T2API
         return Ember.RSVP.hash({
           skills: $.get(App.API_BASE_URL + '/skills'),
-          people: $.get(App.API_BASE_URL + '/people'),
-          person_id: params.neon
+          person: $.get(App.API_BASE_URL + '/people/' + params.neon)
         });
       },
 
       setupController: function (controller, model) {
-        var people = model.people.people.map(App.Raw2EmberObject)
-          , person = people.filterBy('id', parseInt(model.person_id, 10)).get('firstObject');
-        controller.set('person', person);
+        controller.set('person', App.Raw2EmberObject(model.person.person));
         controller.set('skills', model.skills);
       }
 
