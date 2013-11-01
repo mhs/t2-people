de2892301b1291aacd8bd6643db661c1072fdbaf
banafederico <federico@neo.com>
2013-11-01 16:02:13 -0200

    Fix links in profile and moves parse logic to the view.

--- app/js/controllers/profile_controller.js
+++ app/js/controllers/profile_controller.js
@@ -1,30 +1,9 @@
 define(['application'], function (App) {
   App.ProfileController = Ember.Controller.extend({
-
     init: function () {
       this.set('editingSkills', false);
     },
 
     editable: true,
-
-    personData: (function () {
-      var properties = this.get('person').getProperties([
-        'name', 'title', 'bio', 'website', 'github', 'twitter', 'skill_list'
-      ]);
-      return Ember.Object.create(properties);
-    }).property('person'),
-
-    emailLink: (function () {
-      return 'mailto:' + this.get('person.email');
-    }).property('person.email'),
-
-    githubLink: (function () {
-      return 'http://github.com/' + this.get('person.github');
-    }).property('person.github'),
-
-    twitterLink: (function () {
-      return 'http://twitter.com/' + this.get('person.twitter');
-    }).property('person.twitter')
-
   });
 });
