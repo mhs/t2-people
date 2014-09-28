a28f05a74daadbf2500492b1c5ad254bc34074a5
Christopher Westra <bestra@Bestra.local>
2014-09-28 11:53:43 -0400

    remove debugger from easy-form

--- t2-people/vendor/js/ember-easyForm.js
+++ t2-people/vendor/js/ember-easyForm.js
@@ -565,7 +565,6 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 
 
 (function() {
-debugger
 Ember.TEMPLATES['easy-form/input-controls'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
