e9bcdc1751e9680b0e728760c12020ed9d928261
Sara Gibbons <saragibby@gmail.com>
2014-12-08 15:02:24 -0500

    add content security for each environment

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,14 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+      'img-src': "'self' https://t2-data.s3.amazonaws.com/ http://s3.amazonaws.com/",
+      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
+      'media-src': "'self'"
     }
   };
 
@@ -27,6 +35,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.apiHost = "http://localhost:5000"
     ENV.navBarUrl = "/api/v1/navbar"
+    ENV.contentSecurityPolicy['connect-src'] = "'self' http://localhost:5000";
   }
 
   if (environment === 'test') {
@@ -39,11 +48,14 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.contentSecurityPolicy['connect-src'] = "'self'";
   }
 
   if (environment === 'production') {
     ENV.apiHost = "http://t2.neo.com"
     ENV.navBarUrl = "/api/v1/navbar"
+
+    ENV.contentSecurityPolicy['connect-src'] = "'self' https://api.mixpanel.com http://custom-api.local";
   }
 
   return ENV;
