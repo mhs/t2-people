2e8b4fcdbc79f0af10799408a892d29a66ef329a
Jerry Nummi <jerry@mutuallyhuman.com>
2015-04-24 11:12:09 -0400

    WIP

--- app/initializers/ember-easyform.coffee
+++
@@ -1,9 +0,0 @@
-`import Ember from "ember";`
-
-formInitializer =
-  name: 'easyForm'
-  initialize: (container, app) ->
-    options = {}
-    Ember.EasyForm.Config.registerWrapper('default', options)
-
-`export default formInitializer;`
---
+++ app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+export default Router.map(function() {
+  this.route('sign_out');
+
+  this.resource('people');
+  this.route('people.new', { path: '/people/new' });
+
+  this.resource('person', { path: '/people/:person_id' }, function() {
+    this.route('edit');
+  });
+});
